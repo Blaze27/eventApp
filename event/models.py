acf05e2cf7df0c16cbde520ac38a5439161d82c6
Rahul Sharma <bbblaze27@gmail.com>
2021-04-22 12:47:03 +0530

    Create Event and EventTime models

--- event/models.py
+++ event/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from taggit.managers import TaggableManager
+from places.models import Place
+from django.contrib.auth.models import User
 
-# Create your models here.
+from model_utils.models import TimeStampedModel
+# from datetime import datetime, timedelta
+
+
+TAGGIT_CASE_INSENSITIVE = True
+
+class Event(TimeStampedModel):
+    title = models.CharField(max_length=200)
+    place = models.ForeignKey(Place, default=None, on_delete=models.CASCADE)
+    tags = TaggableManager()
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+    
+
+
+class EventTime(models.Model):
+    event_date = models.DateField(null=True)
+    event_time = models.TimeField(null=True)
+    event = models.ForeignKey('Event', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} {}'.format(self.event_time, self.event_date)
+
+
+    
+
